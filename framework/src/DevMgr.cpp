0d91f67f2e8a1ae91a5c853d5cae8b30e67a635e
Mark Charlebois <charlebm@gmail.com>
2015-11-10 07:41:14 -0800

    Fixed write buffer to be const void *
    Signed-off-by: Mark Charlebois <charlebm@gmail.com>

--- framework/src/DevMgr.cpp
+++ framework/src/DevMgr.cpp
@@ -374,7 +374,7 @@ ssize_t DevHandle::read(void *buf, size_t len)
 	}
 	return -1;
 }
-ssize_t DevHandle::write(void *buf, size_t len)
+ssize_t DevHandle::write(const void *buf, size_t len)
 {
 	if (m_handle) {
 		return reinterpret_cast<DevObj *>(m_handle)->devWrite(buf, len);
