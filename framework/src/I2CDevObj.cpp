4f7439be4801aae645be44d285944bb119b3e798
Julian Oes <julian@oes.ch>
2016-02-15 13:39:27 +0100

    I2CDevObj: corrected wrong variable name

--- framework/src/I2CDevObj.cpp
+++ framework/src/I2CDevObj.cpp
@@ -117,11 +117,11 @@ int I2CDevObj::_readReg(uint8_t address, uint8_t *out_buffer, int length)
 	ioctl_write_read.write_buf_len = 1;
 	ioctl_write_read.read_buf = out_buffer;
 	ioctl_write_read.read_buf_len = length;
-	int bytes_written = ::ioctl(m_fd, I2C_IOCTL_RDWR, &ioctl_write_read);
-	if (bytes_written != length) {
+	int bytes_read = ::ioctl(m_fd, I2C_IOCTL_RDWR, &ioctl_write_read);
+	if (bytes_read != length) {
 		DF_LOG_ERR(
 			"error: read register reports a read of %d bytes, but attempted to set %d bytes",
-			bytes_written, length);
+			bytes_read, length);
 		return -1;
 	}
 
