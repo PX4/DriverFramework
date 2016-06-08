cecbffe7060c8982ddf83e778003b3b7c497937f
Michael Schaeuble <schaeuble.michael@gmail.com>
2016-06-08 11:22:23 +0200

    Setup bebop build

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ define df_cmake_generate
 mkdir -p build_$(1) && cd build_$(1) && cmake .. -DOS=$(2) -DCMAKE_TOOLCHAIN_FILE=$(3) -DDF_ENABLE_TESTS=1
 endef
 
-rpi2 linux:
+rpi2 linux bebop:
 	$(call df_cmake_generate,$@,posix,cmake/toolchains/Toolchain-$@.cmake)
 	cd build_$@ && make
 
---
+++ cmake/toolchains/Toolchain-bebop.cmake
@@ -0,0 +1,39 @@
+############################################################################
+# Cross-compilation Toolchain File (CMAKE_TOOLCHAIN_FILE)
+# for Parrot Bebop
+#     requires a proper toolchain setup:
+#     https://github.com/pixhawk/rpi_toolchain
+#
+#
+############################################################################
+add_definitions(
+	-D__BEBOP
+	)
+
+######### test DriverFramework for bebop ###
+# used for debug
+#add_definitions(-DDF_DEBUG)
+
+if ("${RPI_TOOLCHAIN_DIR}" STREQUAL "")
+	set(RPI_TOOLCHAIN_DIR /opt/rpi_toolchain)
+endif()
+
+# this one is important
+set(CMAKE_SYSTEM_NAME Linux)
+
+# this one not so much
+set(CMAKE_SYSTEM_VERSION 1)
+
+# set cross compilers
+set(CMAKE_C_COMPILER "${RPI_TOOLCHAIN_DIR}/gcc-linaro-arm-linux-gnueabihf-raspbian/bin/arm-linux-gnueabihf-gcc")
+set(CMAKE_CXX_COMPILER "${RPI_TOOLCHAIN_DIR}/gcc-linaro-arm-linux-gnueabihf-raspbian/bin/arm-linux-gnueabihf-g++")
+set(CMAKE_C_FLAGS "-static" CACHE STRING "" FORCE)
+set(CMAKE_CXX_FLAGS "-static" CACHE STRING "" FORCE)
+set(LINKER_FLAGS "-Wl,-gc-sections")
+set(CMAKE_EXE_LINKER_FLAGS ${LINKER_FLAGS})
+
+# search for programs in the build host directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# for libraries and headers in the target directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
