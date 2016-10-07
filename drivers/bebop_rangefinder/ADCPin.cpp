8d32f650c5adef90dbb65c6524c0d4cabb5ae6f2
Michael Schaeuble <schaeuble.michael@gmail.com>
2016-10-07 19:46:37 +0200

    BebopRangeFinder: Add simple ADC interface

---
+++ drivers/bebop_rangefinder/ADCPin.cpp
@@ -0,0 +1,160 @@
+/****************************************************************************
+ *
+ *   Copyright (C) 2016 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <poll.h>
+
+#include <time.h>
+#include <errno.h>
+#include <string.h>
+
+#include "ADCPin.hpp"
+#include "DriverFramework.hpp"
+
+using namespace DriverFramework;
+
+ADCPin::ADCPin(uint16_t device_id, uint16_t channel, uint16_t buffer_length)
+  : m_dev_id(device_id), m_dev_path{0}, m_channel(channel), m_buffer_length(buffer_length), m_buffer_enabled(false)
+{
+
+  // Prepare the device path
+  snprintf(m_dev_path, sizeof(m_dev_path), "/sys/bus/iio/devices/iio:device%d", m_dev_id);
+  DF_LOG_INFO("Initialize device: %s", m_dev_path);
+
+  // Before we setup the device, disable the pin
+  disable();
+
+  // Enable the channel
+  char path[35] = {0};
+  snprintf(path, sizeof(path), "scan_elements/in_voltage%d_en", channel);
+  write(path, 1);
+
+  // Set buffer length and disable it initially
+  write("/buffer/length", buffer_length);
+  disable();
+}
+  
+ADCPin::~ADCPin()
+{
+  // Make sure you disable device at the end
+  disable();
+}
+
+int ADCPin::write(const char *path, int value)
+{
+  char filename[sizeof(m_dev_path) + 20] = {0};
+  snprintf(filename, sizeof(filename), "%s/%s", m_dev_path, path);
+
+  FILE *pFile = fopen(filename, "w");
+
+  if (pFile == NULL)
+  {
+    DF_LOG_ERR("Unable to open file: %s", filename);
+    return -1;
+  }
+
+  fprintf(pFile, "%d", value);
+
+  if (fclose(pFile) != 0)
+  {
+    DF_LOG_ERR("Unable to close file: %s", filename);
+    return -1;
+  }
+  return 0;
+}
+
+int ADCPin::enable()
+{
+  m_buffer_enabled = true;
+  return write("buffer/enable", 1);
+}
+  
+int ADCPin::disable()
+{
+  m_buffer_enabled = false;
+  return write("buffer/enable", 0);
+}
+
+int ADCPin::read(uint16_t *buffer, uint16_t len)
+{
+
+  if (!m_buffer_enabled)
+  {
+    DF_LOG_ERR("Read not possible, buffer was not enabled");
+    return -1;
+  }
+
+  FILE *pFile;
+  char path[20];
+  snprintf(path, sizeof(path), "/dev/iio:device%d", m_dev_id);
+
+  pFile = fopen(path, "r");
+
+  if (pFile == NULL)
+  {
+    DF_LOG_ERR("%s", strerror(errno));
+    DF_LOG_ERR("Unable to open file: %s", path);
+    return -1;
+  }
+
+  struct timespec tt;
+  if (clock_gettime(CLOCK_MONOTONIC, &tt) < 0)
+  {
+    DF_LOG_INFO("clock error");
+  }
+  long start_time = tt.tv_nsec;
+  int result = ::fread(buffer, 2, len, pFile);
+  if (result < 0)
+  {
+    DF_LOG_ERR("Error reading: %s", strerror(errno));
+  }
+  if (clock_gettime(CLOCK_MONOTONIC, &tt) < 0)
+  {
+    DF_LOG_INFO("clock error 2");
+  }
+  DF_LOG_INFO("Read took %f", double(tt.tv_nsec - start_time)*1e-6);
+
+  int res = fclose(pFile);
+  if (res != 0)
+  {
+    DF_LOG_ERR("Unable to close file: %s", strerror(errno));
+  }
+
+
+  return 0;
+}
