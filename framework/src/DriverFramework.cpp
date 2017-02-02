ef27fc619c1c3113eb8943a0df8d3538416a358a
wangxdflight <wxiaodong@gmail.com>
2017-02-01 17:10:41 -0800

    fix code style

--- framework/src/DriverFramework.cpp
+++ framework/src/DriverFramework.cpp
@@ -484,9 +484,11 @@ void HRTWorkQueue::process()
 		now = offsetTime();
 		DF_LOG_DEBUG("now=%" PRIu64, now);
 #ifdef __DF_QURT
-                // to accomodate sleep inaccuracy in the platform
+
+		// to accomodate sleep inaccuracy in the platform
 		if (next > now + 200) {
 #else
+
 		if (next > now) {
 #endif
 			uint64_t wait_time_usec = next - now;
