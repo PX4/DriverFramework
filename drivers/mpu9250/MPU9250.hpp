a5bc35577831c749d5c1b025f8651d25c406fdbb
crossa <421395590@qq.com>
2017-03-11 16:48:07 +0800

    set spi frequency to 5M when read sensor data when using raspberry pi as pilot
    set sample rate of compass  to 184hz
    
    If using 10M speed to communicate with spi,
    
    it will cause fifo occupatied and fifo overflow。
    
    SPI frequency should be 1/4 of max frequency
    of mcu.
    
    The max frequency is 20M ,so the speed should be 5M

--- drivers/mpu9250/MPU9250.hpp
+++ drivers/mpu9250/MPU9250.hpp
@@ -181,6 +181,9 @@ namespace DriverFramework
 #if defined(__DF_EDISON)
 // update frequency 250 Hz
 #define MPU9250_MEASURE_INTERVAL_US 4000
+#elif defined(__DF_RPI_SINGLE)
+// update frequency 400 Hz
+#define MPU9250_MEASURE_INTERVAL_US 1000
 #else
 // update frequency 1000 Hz
 #define MPU9250_MEASURE_INTERVAL_US 1000
@@ -240,6 +243,8 @@ public:
 		_mag_enabled(mag_enabled),
 #if defined(__DF_EDISON)
 		_packets_per_cycle_filtered(4.0f), // The FIFO is supposed to run at 1kHz and we sample at 250Hz.
+#elif defined(__DF_RPI_SINGLE)
+		_packets_per_cycle_filtered(8.0f),// The FIFO is supposed to run at 1kHz and we sample at 250Hz.
 #else
 		_packets_per_cycle_filtered(8.0f), // The FIFO is supposed to run at 8kHz and we sample at 1kHz.
 #endif
