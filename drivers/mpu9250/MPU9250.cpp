f22eabddc3eeb0c0f1fc3863e0b82b08a7dbd45a
Fan.Zhang <421395590@qq.com>
2017-03-16 15:06:14 +0800

    Change SPI  frequency setting (#189)
    * Add  marcro definition MPU_WHOAMI_9250_REAL 0x73.
    Change  sensor_id verification,both 0x71 and 0x73 are valid.
    There are various kind of mpu9250 module in China.
    The WHOAMI value of the MPU9250 sensor which I used  was 0x73.
    I don't know why there are different WHOAMI value of them.
    So I add the 0x73 support into the code.
    
    * Add __DF_RPI_SINGLE definition to fix device path.
    I use the rpi to build the autopilot without pxfmini or navio.
    Pxfmini and navio was not popular in China. I cant hardly to purchase it.
    So I use raspberry pi separately.
    If use raspberry pi separately
    and communicate with  MPU9250 sensor by SPI mode,
    both IMU_DEVICE_ACC_GYRO and  IMU_DEVICE_MAG  should be defined as
    "/dev/spidev0.1",other wise the px4firmware can't work correctly.
    
    * Fix code style
    
    * Fix WHOAMI verification
    
    * Fix error
    
    * fixed code style
    
    * Add Ignore
    
    * set spi frequency to 5M when read sensor data when using raspberry pi as
    pilot
    
    set sample rate of compass  to 184hz
    
    If using 10M speed to communicate with spi,
    
    it will cause fifo occupatied and fifo overflow。
    
    SPI frequency should be 1/4 of max frequency
    of mcu.
    
    The max frequency is 20M ,so the speed should be 5M
    
    * format code
    
    * Remove _packets_per_cycle_filtered(8.0f) for __DF_RPI_SINGLE definition
    
    * Revert CMakeLists.txt .
    
    * Fix code style
    
    * Update .gitignore
    
    * revert submodule

--- drivers/mpu9250/MPU9250.cpp
+++ drivers/mpu9250/MPU9250.cpp
@@ -167,6 +167,9 @@ int MPU9250::mpu9250_init()
 	//Therefore, we use the gyro bandwith of 184 Hz which corresponds to 1kHz sampling frequency.
 	result = _writeReg(MPUREG_CONFIG,
 			   BITS_DLPF_CFG_184HZ | BITS_CONFIG_FIFO_MODE_OVERWRITE);
+#elif defined(__DF_RPI_SINGLE)
+	result = _writeReg(MPUREG_CONFIG,
+			   BITS_DLPF_CFG_184HZ | BITS_CONFIG_FIFO_MODE_OVERWRITE);
 #else
 	result = _writeReg(MPUREG_CONFIG,
 			   BITS_DLPF_CFG_250HZ | BITS_CONFIG_FIFO_MODE_OVERWRITE);
@@ -453,6 +456,8 @@ void MPU9250::_measure()
 
 #if defined(__DF_EDISON)
 	//FIFO corrupt at 10MHz.
+	_setBusFrequency(SPI_FREQUENCY_5MHZ);
+#elif defined(__DF_RPI_SINGLE)
 	_setBusFrequency(SPI_FREQUENCY_5MHZ);
 #else
 	_setBusFrequency(SPI_FREQUENCY_10MHZ);
--- drivers/mpu9250/MPU9250.hpp
+++ drivers/mpu9250/MPU9250.hpp
@@ -181,6 +181,9 @@ namespace DriverFramework
 #if defined(__DF_EDISON)
 // update frequency 250 Hz
 #define MPU9250_MEASURE_INTERVAL_US 4000
+#elif defined(__DF_RPI_SINGLE)
+// update frequency 1000 Hz,if using rpi1,rpi zero,1000hz may be to higher,please reduce the frequency
+#define MPU9250_MEASURE_INTERVAL_US 1000
 #else
 // update frequency 1000 Hz
 #define MPU9250_MEASURE_INTERVAL_US 1000
