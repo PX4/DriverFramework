15df94e0843ade8ff047ec56f823d471da2afa13
crossa <421395590@qq.com>
2017-06-24 02:02:37 +0800

    参数回滚

--- drivers/mpu9250/MPU9250.hpp
+++ drivers/mpu9250/MPU9250.hpp
@@ -182,8 +182,6 @@ namespace DriverFramework
 #if defined(__DF_EDISON)
 // update frequency 250 Hz
 #define MPU9250_MEASURE_INTERVAL_US 4000
-#elif defined(__DF_ARM_GENERIC)
-#define MPU9250_MEASURE_INTERVAL_US 2500
 #elif defined(__DF_RPI_SINGLE)
 // update frequency 1000 Hz,if using rpi1,rpi zero,1000hz may be to higher,please reduce the frequency
 #define MPU9250_MEASURE_INTERVAL_US 1000
@@ -246,8 +244,6 @@ public:
 		_mag_enabled(mag_enabled),
 #if defined(__DF_EDISON)
 		_packets_per_cycle_filtered(4.0f), // The FIFO is supposed to run at 1kHz and we sample at 250Hz.
-#elif defined(__DF_ARM_GENERIC)
-		_packets_per_cycle_filtered(20.0f),
 #else
 		_packets_per_cycle_filtered(8.0f), // The FIFO is supposed to run at 8kHz and we sample at 1kHz.
 #endif
