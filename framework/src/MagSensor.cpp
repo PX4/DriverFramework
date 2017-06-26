90c24bd910539b955779fc435ded4d8ada777f86
Nicolae Rosia <nicolae.rosia@gmail.com>
2017-06-26 08:30:52 +0300

    MagSensor: fix style
    Signed-off-by: Nicolae Rosia <nicolae.rosia@gmail.com>

--- framework/src/MagSensor.cpp
+++ framework/src/MagSensor.cpp
@@ -39,9 +39,9 @@ namespace DriverFramework
 void printMagValues(struct mag_sensor_data &data)
 {
 	DF_LOG_INFO("Mag: [%.6f, %.6f, %.6f] Ga",
-			(double)data.field_x_ga,
-			(double)data.field_y_ga,
-			(double)data.field_z_ga);
+		    (double)data.field_x_ga,
+		    (double)data.field_y_ga,
+		    (double)data.field_z_ga);
 }
 
 }
